a0c4020e51538e12ca6aa2b30306c139085059ba
BrainWave <pikadon92@gmail.com>
2025-06-29 00:53:09 +0800

    Have FileDialog Opened

--- source/app.d
+++ source/app.d
@@ -1,14 +1,31 @@
-import dlangui;
+import dlangui.dialogs.filedlg;
+import dlangui.widgets.controls;
+import dlangui.widgets.layouts;
+import dlangui.widgets.widget;
 
 mixin APP_ENTRY_POINT;
 
 /// entry point for dlangui based application
 extern (C) int UIAppMain(string[] args) {
     // create window
-    Window window = Platform.instance.createWindow("DlangUI example - HelloWorld", null);
+    Window window = Platform.instance.createWindow("Krita Layers", null);
 
-    // create some widget to show in window
-    window.mainWidget = (new Button()).text("Hello, world!"d).margins(Rect(20,20,20,20));
+    // create some widget
+    auto vlayout = new VerticalLayout();
+
+    UIString caption = "Open File"d;
+    FileDialog fileDialog = new FileDialog(caption, window);
+
+    Button kritBtn = new Button();
+    kritBtn.text("Open Krita"d);
+    kritBtn.click = delegate(Widget src) {
+        fileDialog.show();
+        return true;
+    };
+
+    // show them in window
+    vlayout.addChild(kritBtn);
+    window.mainWidget = vlayout;
 
     // show window
     window.show();
