f9d1cf5b4ce85b476e784ddd4535427ea8f00dda
BrainWave <pikadon92@gmail.com>
2025-06-30 19:25:01 +0800

    Read File

--- source/app.d
+++ source/app.d
@@ -1,6 +1,17 @@
+import std.file;
+import std.path;
 import std.stdio;
 
 void main()
 {
-    writeln("Hello, world!");
+    writeln("Enter a krita file:");
+    //auto file = File(readln());
+    auto file = File("example.txt");
+    writeln(file.name);
+
+    if (extension(file.name) == ".kra"){
+        writeln("Success!");
+    } else {
+        writeln("This is not a krita file.");
+    };
 }
